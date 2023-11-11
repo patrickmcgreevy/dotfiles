beda8ed8a4d82431232bc09978f361d2197255c1
Patrick McGreevy <patrick.mcgreevy99@gmail.com>
2023-11-11 13:49:11 -0800

    Adds trouble plugin

--- .config/nvim/lua/theprimeagen/packer.lua
+++ .config/nvim/lua/theprimeagen/packer.lua
@@ -1,47 +1,72 @@
 vim.cmd.packadd('packer.nvim')
 
 return require('packer').startup(function(use)
-    -- Packer can manage itself
-    use 'wbthomason/packer.nvim'
+	-- Packer can manage itself
+	use 'wbthomason/packer.nvim'
 
-    use {
-        'nvim-telescope/telescope.nvim', tag = '0.1.3',
-        -- or                            , branch = '0.1.x',
-        requires = { {'nvim-lua/plenary.nvim'} }
-    }
+	use {
+		'nvim-telescope/telescope.nvim', tag = '0.1.3',
+		-- or                            , branch = '0.1.x',
+		requires = { {'nvim-lua/plenary.nvim'} }
+	}
 
-    use({ 'rose-pine/neovim', as = 'rose-pine' })
-    use("theprimeagen/harpoon")
-    use("theprimeagen/refactoring.nvim")
-    use("tpope/vim-fugitive")
-    use("mbbill/undotree")
-    use({"nvim-treesitter/nvim-treesitter", run = ":TSUpdate"})
-    use {
-        'numToStr/Comment.nvim',
-        config = function()
-            require('Comment').setup()
-        end
-    }
-    use {
-        'VonHeikemen/lsp-zero.nvim',
-        branch = 'v1.x',
+	use({ 'rose-pine/neovim', as = 'rose-pine' })
+	use("theprimeagen/harpoon")
+	use("theprimeagen/refactoring.nvim")
+	use("tpope/vim-fugitive")
+	use("mbbill/undotree")
+	use({"nvim-treesitter/nvim-treesitter", run = ":TSUpdate"})
+	use {
+		'numToStr/Comment.nvim',
+		config = function()
+			require('Comment').setup()
+		end
+	}
+	use({
+		"folke/trouble.nvim",
+		config = function()
+			require("trouble").setup {
+				icons = false,
+				fold_open = "v", -- icon used for open folds
+					fold_closed = ">", -- icon used for closed folds
+					indent_lines = false, -- add an indent guide below the fold icons
+					signs = {
+						-- icons / text used for a diagnostic
+						error = "error",
+						warning = "warn",
+						hint = "hint",
+						information = "info"
+					},
+				use_diagnostic_signs = false -- enabling this will use the signs defined in your lsp client
+				-- your configuration comes here
+				-- or leave it empty to use the default settings
+				-- refer to the configuration section below
+			}
+		end,
         requires = {
-            -- LSP Support
-            {'neovim/nvim-lspconfig'},
-            {'williamboman/mason.nvim'},
-            {'williamboman/mason-lspconfig.nvim'},
-
-            -- Autocompletion
-            {'hrsh7th/nvim-cmp'},
-            {'hrsh7th/cmp-buffer'},
-            {'hrsh7th/cmp-path'},
-            {'saadparwaiz1/cmp_luasnip'},
-            {'hrsh7th/cmp-nvim-lsp'},
-            {'hrsh7th/cmp-nvim-lua'},
-
-            -- Snippets
-            {'L3MON4D3/LuaSnip'},
-            {'rafamadriz/friendly-snippets'},
+            {'nvim-tree/nvim-web-devicons'}
         }
-    }
+	})
+	use {
+		'VonHeikemen/lsp-zero.nvim',
+		branch = 'v1.x',
+		requires = {
+			-- LSP Support
+			{'neovim/nvim-lspconfig'},
+			{'williamboman/mason.nvim'},
+			{'williamboman/mason-lspconfig.nvim'},
+
+			-- Autocompletion
+			{'hrsh7th/nvim-cmp'},
+			{'hrsh7th/cmp-buffer'},
+			{'hrsh7th/cmp-path'},
+			{'saadparwaiz1/cmp_luasnip'},
+			{'hrsh7th/cmp-nvim-lsp'},
+			{'hrsh7th/cmp-nvim-lua'},
+
+			-- Snippets
+			{'L3MON4D3/LuaSnip'},
+			{'rafamadriz/friendly-snippets'},
+		}
+	}
 end)
