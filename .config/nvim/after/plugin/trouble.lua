09e2955a93cdb4be0308c13129bcd24762d5b7c3
Patrick McGreevy <patrick.mcgreevy99@gmail.com>
2023-11-11 13:56:02 -0800

    Use API instead of commands

--- .config/nvim/after/plugin/trouble.lua
+++ .config/nvim/after/plugin/trouble.lua
@@ -1,6 +1,6 @@
 vim.keymap.set("n", "<leader>xq", "<cmd>TroubleToggle quickfix<cr>",
     {silent = true, noremap = true}
 )
-vim.keymap.set("n", "<leader>vad", "<cmd>TroubleToggle document_diagnostics<cr>",
+vim.keymap.set("n", "<leader>vad",function() require("trouble").toggle("document_diagnostics") end,
     {silent = true, noremap = true}
 )
