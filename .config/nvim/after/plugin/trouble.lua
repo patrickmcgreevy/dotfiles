b7ffb53f4ae7048f700c0049205fd2edef681fdd
Patrick McGreevy <patrick.mcgreevy99@gmail.com>
2023-11-23 10:31:27 -0800

    Improves trouble config
    Makes trouble and telescope work better together.

--- .config/nvim/after/plugin/trouble.lua
+++ .config/nvim/after/plugin/trouble.lua
@@ -1,4 +1,4 @@
-vim.keymap.set("n", "<leader>xq", "<cmd>TroubleToggle quickfix<cr>",
+vim.keymap.set("n", "<leader>tt",require("trouble").toggle,
     {silent = true, noremap = true}
 )
 vim.keymap.set("n", "<leader>vad",function() require("trouble").toggle("document_diagnostics") end,
